5d9c4e895ba255b56d213e0e7c41fb8160148691
Thiago Auler Wideck <aulerwideck@gmail.com>
2017-06-15 15:18:22 -0300

    a

--- APS_DB/Banco.cs
+++ APS_DB/Banco.cs
@@ -21,23 +21,155 @@ namespace APS_DB
         {
             mConn = new MySqlConnection("Persist Security Info=False;server=" + ip + ";database=;uid=" + user + ";server=" + ip + ";database=;uid=" + user + ";pwd=" + senha);
         }
-        
+
         //select ***
-        public DataTable getPaises()
+        public DataTable getCidades()
         {
             if (mConn.State == ConnectionState.Open)
             {
                 DataTable dt = new DataTable();
-                mAdapter = new MySqlDataAdapter("SELECT * FROM Pais ", mConn);
+                mAdapter = new MySqlDataAdapter("SELECT * FROM cidade", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getEnderecos()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM endereco", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getEstados()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM estado", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getFretes()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM frete", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getFrete_Veiculo()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM frete_veiculo ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getMarca()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM marca ", mConn);
                 mAdapter.Fill(dt);
                 return dt;
             }
             return null;
         }
 
+        public DataTable getModeelos()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM modelo ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getPaises()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM pais ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getPessoa()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM pessoa ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getTelefone()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM telefone ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getTipoPessoa()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM tipoPessoa ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getTipoVeiculo()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM tipoVeiculo ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
+        public DataTable getVeiculos()
+        {
+            if (mConn.State == ConnectionState.Open)
+            {
+                DataTable dt = new DataTable();
+                mAdapter = new MySqlDataAdapter("SELECT * FROM veiculo ", mConn);
+                mAdapter.Fill(dt);
+                return dt;
+            }
+            return null;
+        }
         //inserts
 
-            
-              
+
+
     }
 }
