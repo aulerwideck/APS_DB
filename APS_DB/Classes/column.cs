9d09e1772dca23fb58d960ef477eaffd56acd81b
Thiago Auler Wideck <aulerwideck@gmail.com>
2017-06-17 20:56:47 -0300

    corrigido identação

--- APS_DB/Classes/column.cs
+++ APS_DB/Classes/column.cs
@@ -7,9 +7,9 @@ using System.Windows.Forms;
 
 namespace APS_DB.Classes
 {
-	public class column
-	{
-		private string name;
+    public class column
+    {
+        private string name;
         private string friendlyName;
         private string fkTableName;
         private bool isFK;
@@ -19,26 +19,32 @@ namespace APS_DB.Classes
         private MySqlType type;
         private int size;
 
-		private Control control;
+        private Control control;
 
-		public Control Control { get { return control; } set { control = value; } }
-		public string Name { get { return name; } set { name = value; } }
-
-        public string FriendlyName { get => friendlyName; set => friendlyName = value; }
-        public bool InsertReq { get => insertReq; set => insertReq = value; }
-        public bool IsFk { get => isFK; set => isFK = value; }
-        public string FkTableName { get => fkTableName; set => fkTableName = value; }
-        public bool IsSearchField { get => isSearchField; set => isSearchField = value; }
-        internal MySqlType Type { get => type; set => type = value; }
-        public int Size { get => size; set => size = value; }
-        public bool IsPrimaryKey { get => isPrimaryKey; set => isPrimaryKey = value; }
-
+        public string Name
+        { get { return name; } set { name = value; } }
+        public string FriendlyName
+        { get { return friendlyName; } set { friendlyName = value; } }
+        public string FkTableName
+        { get { return fkTableName; } set { fkTableName = value; } }
+        public bool IsFK
+        { get { return isFK; } set { isFK = value; } }
+        public bool InsertReq
+        { get { return insertReq; } set { insertReq = value; } }
+        public bool IsSearchField
+        { get { return isSearchField; } set { isSearchField = value; } }
+        public bool IsPrimaryKey
+        { get { return isPrimaryKey; } set { isPrimaryKey = value; } }
+        public MySqlType Type
+        { get { return type; } set { type = value; } }
+        public int Size
+        { get { return size; } set { size = value; } }
         public column(column c)
         {
             name = c.name;
             friendlyName = c.friendlyName;
             fkTableName = c.fkTableName;
-            IsFk = c.isFK;
+            isFK = c.isFK;
             insertReq = c.insertReq;
             isSearchField = c.isSearchField;
             isPrimaryKey = c.isPrimaryKey;
@@ -52,12 +58,12 @@ namespace APS_DB.Classes
         public column(string name, string friendlyName = null, bool insertReq = true, MySqlType type = MySqlType.notNeeded, int size = -1, bool isSearchField = false, bool? isFk = null, string fkTableName = null, bool isPrimaryKey = false)
         {
             Name = name;
-            FriendlyName = string.IsNullOrEmpty(friendlyName) ? name: friendlyName;
+            FriendlyName = string.IsNullOrEmpty(friendlyName) ? name : friendlyName;
             InsertReq = insertReq;
             Type = type;
             Size = size;
             IsSearchField = isSearchField;
-            IsFk = isFk.HasValue ? isFk.Value : false;
+            IsFK = isFk.HasValue ? isFk.Value : false;
             FkTableName = fkTableName;
             IsPrimaryKey = isPrimaryKey;
         }
